9ab32bb5be3f2888c51b75f583b748652d8147ca
Nasir Uddin <nasir200087@diit.edu.bd>
2024-11-05 17:54:43 +0600

    new added

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -129,12 +129,11 @@
             items: 3,
             loop: true,
             autoplay: true,
-            dots: true,
+            dots: false,
             margin: 0,
             autoplayTimeout: 4000,
-            nav: true,
+            nav: false,
             smartSpeed: 800,
-            navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
             responsive: {
                 0: {
                     items: 1 
